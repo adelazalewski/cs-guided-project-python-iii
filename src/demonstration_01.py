43e4c9badb94044dea827bdd086ae4aa4d53183a
adelazalewski <paliuadela@yahoo.com>
2021-04-12 22:03:47 -0400

    finished guided project

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -13,6 +13,18 @@ Notes:
 - You may not use the same element twice.
 - You can return the answer in any order.
 """
-def two_sum(nums, target):
+def two_sum(numbers, target):
     # Your code here
+    target_nums = []
+    for num1 in numbers:
+        for num2 in numbers:
+            if(num1 == num2):
+                continue
+            elif(num1 + num2 == target):
+                target_nums.append(numbers.index(num1))
+                #target_nums.append(numbers.index(num2))
+    return target_nums
 
+print(two_sum([2,5,9,13], 7))
+print(two_sum([4,3,5], 8))
+print()
